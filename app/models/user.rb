61b09674dd157e7a922879ea269b695d5641dbe9
thanhmancity <mahoangtienthanh@gmail.com>
2018-04-05 00:01:12 +0700

    Init app

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+      password = Devise.friendly_token[0,20]
+      user = User.create(name: data["name"], email: data["email"],
+        encrypted_password: password
+      )
+    end
+    user
+  end
+end
---
+++ db/migrate/20180404164244_add_name_user.rb
@@ -0,0 +1,6 @@
+class AddNameUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :password, :string
+  end
+end
